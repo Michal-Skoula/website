252272372f3b753a49afc7f0e685e0766dff64ea
Michal Škoula <imichalthemichal@gmail.com>
2024-08-28 13:38:49 +0200

    Testimonial display test 4

--- blocks/testimonials/block.php
+++ blocks/testimonials/block.php
@@ -8,52 +8,32 @@ $testimonial_count = esc_attr(get_field('testimonial_count'));
 
 $args = array(
   'post_type' => 'reviews',
-  // 'posts_per_page' => $testimonial_count,
-  'orderby' => 'date',
-  'order' => 'ASC'
+  'posts_per_page' => -1, // Get all posts
 );
 $reviews_query = new WP_Query($args);
-?>
 
-<div id="testimonials" class="section">
-  <div class="max-content-width">
-    <div class="swiper testimonialSwiper">
-      <h2 class="animate fade-in"> <?php echo $heading ?></h2>
-      <!-- Repeater field -->
-      <div class="swiper-wrapper">
-        <?php
-        if($reviews_query->have_posts()) {
-          while($reviews_query->have_posts()) {
-              $reviews_query->the_post();
-              $author = get_field('author');
-              $company = get_field('company');
-              $review_text = get_field('review_text');
+// Check if any reviews were found
+if($reviews_query->have_posts()) {
+  echo '<div class="testimonials-block">';
+  while($reviews_query->have_posts()) {
+      $reviews_query->the_post();
+      $author = get_field('author');
+      $company = get_field('company');
+      $review_text = get_field('review_text');
 
-              echo $reviews_query->the_post();
-              echo $author;
-          ?>
+      // Output the testimonial
+      echo '<div class="testimonial">';
+      echo '<p class="testimonial-text">' . esc_html($review_text) . '</p>';
+      echo '<p class="testimonial-author">' . esc_html($author) . '</p>';
+      echo '<p class="testimonial-company">' . esc_html($company) . '</p>';
+      echo '</div>';
+  }
+  echo '</div>';
 
-
-
-
-
-        <div class="swiper-slide testimonial animate staggered left-to-right noblur">
-          <p> <?php echo $review_text ?></p>
-          <div class="author">
-            <p class="strong"><?php echo $author ?></p>
-            <p> <?php echo ($company) ? $company : '' ?></p>
-          </div>
-        </div>
-        <?php
-          }
-        }
-        else {
-          echo '<p>No content found.</p>';
-        }
-        wp_reset_postdata();
-        ?>
-      </div>
-      <!-- End Repeater field -->
-      <div class="swiper-pagination"></div>
-    </div>
-  </div>
+  // Restore original Post Data
+  wp_reset_postdata();
+} else {
+  // No reviews found
+  echo '<p>No testimonials found.</p>';
+}
+}
